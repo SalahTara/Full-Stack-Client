119087086a4bb16fbab0a01cd733a310fbed4e9a
Salaheddin Altarabichi <salahtarabichi@gmail.com>
2025-07-21 14:31:31 +0300

    First Commit

---
+++ vite-project/src/pages/ProfilePage.jsx
@@ -0,0 +1,68 @@
+import React, { use } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useContext } from "react";
+import { useState } from "react";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import { AuthContext } from "../helpers/AuthContext";
+
+function ProfilePage() {
+  let { id } = useParams();
+  const [username, setUsername] = useState("");
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
+      setUsername(response.data.username);
+    });
+    axios.get(`http://localhost:3001/posts/byUserId/${id}`).then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="profilePageContainer">
+      <div className="basicInfo">
+        <h1>Username: {username}</h1>
+        {authState.username === username && (
+          <button
+            onClick={() => {
+              navigate("/changepassword");
+            }}
+          >
+            Reset Password
+          </button>
+        )}
+      </div>
+      <div className="listOfPosts">
+        {listOfPosts.map((value, key) => {
+          return (
+            <div className="post">
+              <div className="title">{value.title}</div>
+              <div
+                className="body"
+                onClick={() => {
+                  navigate(`/post/${value.id}`);
+                }}
+              >
+                {value.postText}
+              </div>
+              <div className="footer">
+                <div className="username">{value.username}</div>
+                <div className="buttons">
+                  <ThumbUpAltIcon />
+
+                  <label>{value.Likes.length}</label>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
